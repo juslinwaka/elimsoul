dcd67b055f344638ccfdb0bad507ed07e45244dd
Juslin Jay <84858525+juslinwaka@users.noreply.github.com>
2025-04-25 04:37:07 -0700

    Final  Landing page updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,31 @@
 'use client'
 import Link from 'next/link'
 import Image from 'next/image'
-import React from 'react'
+import React, {useState} from 'react'
 import Grid from '@mui/material/Grid'
 import Box from '@mui/material/Box'
 import { useTheme } from '@mui/material/styles'
-import { Button, Typography, TextField } from '@mui/material'
+import { Button, Typography, TextField, 
+  IconButton, InputAdornment } from '@mui/material'
 import { useScreenConfig } from '@/hooks/screenConfig'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import { text } from 'stream/consumers'
 
 export default function Home() {
   const {isMobile, isTablet, isDesktop} = useScreenConfig();
   const theme = useTheme();
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleTogglePassword = () => {
+    setShowPassword(prev => !prev);
+  }
   return (
-    <div className='min-h-screen bg-cover bg-center' >
+    <div className='min-h-screen bg-cover bg-center'>
 
       {isMobile&& 
       <div>
-        <Grid container spacing={2} justifyContent='center' justifyItems='center' justifySelf='center'>
+        <Grid container spacing={2} 
+        justifyContent='center' justifyItems='center' justifySelf='center'>
           <Grid size={9}>
           <Image
             src='/elimsoullogo.png'
@@ -35,16 +44,36 @@ export default function Home() {
 
           <Grid size={8} justifySelf='center'>
             <Box p={1} justifySelf='center'>
-              <TextField 
-                inputMode='email' 
-                size='small' 
-                helperText='Email' color='primary'>Email</TextField>
-            </Box>
 
-            <Box p={1} justifySelf='center'>
-              <TextField 
-                size='small' 
-                color='primary' helperText='Password'> Password</TextField>
+              <form>
+                <TextField 
+                  fullWidth
+                  size='small' 
+                  color='primary'
+                  label='Email'
+                  variant='outlined'
+                  type='email'/>
+
+                <TextField 
+                  size='small' 
+                  fullWidth
+                  label='Password'
+                  color='primary'
+                  type={showPassword ? 'text' : 
+                    'password'}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton onClick={handleTogglePassword} edge='end'>
+                          {showPassword ? <VisibilityOff/> : <Visibility/> }
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                  variant='outlined'/>
+
+              </form>
+              
             </Box>
 
           </Grid>
@@ -90,18 +119,39 @@ export default function Home() {
                    color='textSecondary'>
                   Welcome to ElimSoul
                 </Typography>
-
               </Grid>
 
               <Grid size={8} justifySelf='center'>
             <Box p={1} justifySelf='center'>
-              <TextField inputMode='email' helperText='Email' color='primary'>Email</TextField>
-            </Box>
+            <form>
+                <TextField 
+                  fullWidth
+                  size='small' 
+                  color='primary'
+                  label='Email'
+                  variant='outlined'
+                  type='email'/>
 
-            <Box p={1} justifySelf='center'>
-              <TextField helperText='Password'> Password</TextField>
-            </Box>
+                <TextField 
+                  size='small' 
+                  fullWidth
+                  label='Password'
+                  color='primary'
+                  type={showPassword ? 'text' : 
+                    'password'}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton onClick={handleTogglePassword} edge='end'>
+                          {showPassword ? <VisibilityOff/> : <Visibility/> }
+                        </IconButton>
+                      </InputAdornment>
+                    )
+                  }}
+                  variant='outlined'/>
 
+              </form>
+            </Box>
           </Grid>
 
           <Grid size={8} 
@@ -118,7 +168,7 @@ export default function Home() {
                 <Button 
                   variant='text'
                   color='secondary'
-                  size='large'> Create An Account</Button>
+                  size='large'>Create An Account </Button>
               </Box>
           </Grid>
             </Grid>
---
+++ app/signUp/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import Grid from '@mui/material/Grid'
+
+
+export default function SignUp() {
+  return (
+    <div className='h-fill bg-cover bg-center' >
+    
+        <Grid container spacing={2}>
+     
+      </Grid>
+    </div>
+  );
+}
