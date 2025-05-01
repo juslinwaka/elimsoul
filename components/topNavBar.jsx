c3ace45a6eacba14cc026fa834adfbf02e34187b
Juslin Jay <84858525+juslinwaka@users.noreply.github.com>
2025-05-01 11:02:49 -0700

    New updates

--- components/topNavBar.jsx
+++ components/topNavBar.jsx
@@ -1,9 +1,18 @@
 'use client'
 
 import React from 'react';
-import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import { AppBar, Toolbar, 
+  Typography, Box,
+  IconButton, 
+  List, 
+  ListItem,   
+  useMediaQuery,
+  Menu,
+  Button,
+  MenuItem, Grid } from '@mui/material';
 import Image from 'next/image';
 import { useScreenConfig } from '@/hooks/screenConfig'
+import MenuIcon from '@mui/icons-material/Menu';
 
 const TopNavBar = () => {
 
@@ -13,14 +22,35 @@ const TopNavBar = () => {
     <AppBar position='fixed' color='default' elevation={1}>
       {isMobile &&
         <Toolbar disableGutters style={{minHeight: 30}}>
-          <Image src='/elimsoullogo.png' alt='Logo' width={60} height={60}/>
+          <Grid container spacing={2}>
+            <Grid size={3}>
+              <Box>
+                <IconButton color='inherit'
+                  edge='end'
+                  sx={{display: {xs: 'block', md: 'none'}}}>
+                  <MenuIcon />
+                </IconButton>
+              </Box>
+            </Grid>
 
+            <Grid pl={2} size={3}>
+              <Image pl={2} src='/elimsoulnavlogo.png' alt='Logo' width={40} height={40}/>
+            </Grid>
+
+            <Grid size={3}>
+              <Box justifyContent='center' justifyItems='center' justifySelf='left' pl={4}>
+                
+                <Typography pt={1} justifySelf='center' variant='h6' fontWeight={600}>ElimSoul</Typography>
+              </Box>
+              
+            </Grid>
+          </Grid>
         </Toolbar>
       }
 
       {isDesktop && 
       <Toolbar>
-        <Image src='/elimsoullogo.png' alt='Logo' width={60} height={60}/>
+        <Image src='/elimsoulnavlogo.png' alt='Logo' width={60} height={60}/>
 
         <Typography variant='h5' fontWeight={600}>ElimSoul</Typography>
       </Toolbar>
