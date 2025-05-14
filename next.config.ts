9796eac88f0db4fa3b05627eaf1377103a4eefc2
Juslin Jay <84858525+juslinwaka@users.noreply.github.com>
2025-05-14 12:48:28 -0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-headers() {
-  return Promise.resolve([
-    {
-      source: '/(.*)',
-      headers: [
-        {
-          key: 'Content-Security-Policy',
-          value: "default-src 'self'; script-src 'self' https://*.daily.co; frame-src https://*.daily.co;",
-        },
-      ],
-    },
-  ]);
-}
+
 };
 export default nextConfig;
