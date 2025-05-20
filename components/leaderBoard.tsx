fea20a1d518259c4a2ade996d29a02e5566342d5
Juslin Jay <84858525+juslinwaka@users.noreply.github.com>
2025-05-20 01:53:49 -0700

    Update leaderBoard.tsx

--- components/leaderBoard.tsx
+++ components/leaderBoard.tsx
@@ -20,16 +20,16 @@ const Leaderboard = () => {
                                     <Typography sx={{color: 'white', marginLeft:2}} fontWeight={600} variant='h5'>
                                         Names</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}}>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}}>
                                     <Typography sx={{color: 'white'}} fontWeight={300} variant='h5'>John Devi</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}}>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}}>
                                     <Typography sx={{color: 'white'}} fontWeight={300} variant='h5'>Sarah Amani</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}}>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}}>
                                     <Typography sx={{color: 'white'}} fontWeight={300} variant='h5'>Michael Jumbe</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}}>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}}>
                                     <Typography sx={{color: 'white'}} fontWeight={300} variant='h5'>Judith Mwenda</Typography>
                                   </Box>
                                 </Grid>
@@ -39,16 +39,16 @@ const Leaderboard = () => {
                                   <Box sx={{border: 2, borderColor: 'white', backgroundColor: 'gray'}} justifyItems='end'>
                                     <Typography sx={{color: 'white', marginRight: 2}} fontWeight={600} variant='h5'>Soul Points</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}} justifyItems='end'>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}} justifyItems='end'>
                                     <Typography sx={{color: 'white', marginRight: 3}} fontWeight={400} variant='h5'>4000 ESP</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}} justifyItems='end'>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}} justifyItems='end'>
                                     <Typography sx={{color: 'white', marginRight: 3}} fontWeight={400} variant='h5'>810 ESP</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}} justifyItems='end'>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}} justifyItems='end'>
                                     <Typography sx={{color: 'white', marginRight: 3}} fontWeight={400} variant='h5'>450 ESP</Typography>
                                   </Box>
-                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 20}} justifyItems='end'>
+                                  <Box sx={{border: 2, borderColor: 'white', minHeight: 50}} justifyItems='end'>
                                     <Typography sx={{color: 'white', marginRight: 3}} fontWeight={400} variant='h5'>100 ESP</Typography>
                                   </Box>
                 
